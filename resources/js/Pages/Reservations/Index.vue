3278742389633f3877e88fab190ffb8b54f253c5
Pablo Diaz <pabloandi@gmail.com>
2024-04-25 09:07:07 -0500

    * change datatable component to vue3easydatatable due to create item field templates * allow phone to be a whatsapp link * allow email to be a emailto link

--- resources/js/Pages/Reservations/Index.vue
+++ resources/js/Pages/Reservations/Index.vue
@@ -14,11 +14,38 @@
                     ></CreateButton>
                 </div>
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <DataTable :headers="headers" :items="items">
+                    <Vue3EasyDataTable
+                        v-model:items-selected="itemsSelected"
+                        :headers="headers"
+                        :items="items"
+                    >
                         <template #expand="item">
                             <Expand :item="item" />
                         </template>
-                    </DataTable>
+                        <template #item-phone="{ phone, whatsapp_link }">
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                class="text-blue-500 hover:text-blue-800 cursor-pointer"
+                                :href="whatsapp_link"
+                                v-text="phone"
+                            ></a>
+                        </template>
+                        <template #item-email="{ email }">
+                            <a
+                                class="text-blue-500 hover:text-blue-800 cursor-pointer"
+                                :href="'mailto:' + email"
+                                v-text="email"
+                            ></a>
+                        </template>
+                        <template #item-operation="{ edit_url }">
+                            <div class="operation-wrapper">
+                                <div style="padding: 15px">
+                                    <EditItemButton :action="edit_url" />
+                                </div>
+                            </div>
+                        </template>
+                    </Vue3EasyDataTable>
                 </div>
             </div>
         </div>
@@ -27,14 +54,19 @@
 
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import DataTable from "@/Components/Rentacar/DataTable.vue";
 import CreateButton from "@/Components/Rentacar/CreateButton.vue";
+import EditItemButton from "@/Components/Rentacar/EditItemButton.vue";
 import Expand from "@/Pages/Reservations/Expand.vue";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
+import { ref } from "vue";
 
 const props = defineProps({
     headers: Array,
     items: Array,
 });
+
+const itemsSelected = ref([]);
 </script>
 
 <style lang="scss" scoped></style>
