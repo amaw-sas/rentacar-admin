0d36a39b5a462e880b56ff37286e9938b83b97e2
Pablo Diaz <pabloandi@gmail.com>
2024-09-12 15:41:21 -0500

    modify exceptions origins

--- app/Rentcar/Localiza/ProcessWarning.php
+++ app/Rentcar/Localiza/ProcessWarning.php
@@ -3,18 +3,18 @@
 namespace App\Rentcar\Localiza;
 
 use Illuminate\Support\Arr;
-use App\Rentcar\Localiza\Exceptions\OutOfSchedulePickupDateException;
-use App\Rentcar\Localiza\Exceptions\OutOfSchedulePickupHourException;
-use App\Rentcar\Localiza\Exceptions\OutOfScheduleReturnDateException;
-use App\Rentcar\Localiza\Exceptions\OutOfScheduleReturnHourException;
-use App\Rentcar\Localiza\Exceptions\NoAvailableCategoriesException;
-use App\Rentcar\Localiza\Exceptions\SameHourException;
-use App\Rentcar\Localiza\Exceptions\InferiorPickupDateException;
-use App\Rentcar\Localiza\Exceptions\InferiorReturnDateException;
-use App\Rentcar\Localiza\Exceptions\HolidayPickupDateBranchException;
-use App\Rentcar\Localiza\Exceptions\HolidayReturnDateBranchException;
-use App\Rentcar\Localiza\Exceptions\HolidayOutOfSchedulePickupDateBranchException;
-use App\Rentcar\Localiza\Exceptions\HolidayOutOfScheduleReturnDateBranchException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\OutOfSchedulePickupDateException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\OutOfSchedulePickupHourException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\OutOfScheduleReturnDateException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\OutOfScheduleReturnHourException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\NoAvailableCategoriesException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\SameHourException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\InferiorPickupDateException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\InferiorReturnDateException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\HolidayPickupDateBranchException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\HolidayReturnDateBranchException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\HolidayOutOfSchedulePickupDateBranchException;
+use App\Rentcar\Localiza\Exceptions\VehAvailRate\HolidayOutOfScheduleReturnDateBranchException;
 use App\Rentcar\Localiza\Exceptions\UnknowException;
 
 class ProcessWarning {
