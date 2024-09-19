1ac60b3bfdd55148e7637f7da6d3e368e8e2e47b
Pablo Diaz <pabloandi@gmail.com>
2024-09-19 15:00:22 -0500

    add reservation status "con codigo en revision"

--- app/Enums/ReservationStatus.php
+++ app/Enums/ReservationStatus.php
@@ -7,6 +7,7 @@ enum ReservationStatus: string {
     case SinConfirmar = "Sin confirmar";
     case SinDisponibilidad = "Sin disponibilidad";
     case ConCodigo = "Con código";
+    case ConCodigoEnRevision = "Con código En revisión";
     case Nueva = "Nueva";
     case NoRecogido = "No recogido";
     case ConfirmadoPendientePago = "Confirmado Pendiente Pago";
