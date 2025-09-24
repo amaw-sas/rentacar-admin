e43956b92f6b0a5dbfb3431eb6cb3cfe56519477
Pablo Diaz <pabloandi@gmail.com>
2025-09-24 15:10:57 -0500

    bug: fake all http interactions with Wati when testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,23 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+use Illuminate\Support\Facades\Http;
+use App\Providers\WatiServiceProvider;
+use Mockery;
+use Mockery\MockInterface;
+
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Fake Wati HTTP responses to prevent real API calls
+        $wati_url = config('wati.endpoint');
+        Http::fake([
+            "$wati_url*" => Http::response(['result' => true], 200),
+        ]);
+
+   }
+
 }
